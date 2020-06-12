767675471d8486e6ce600a89752e67ad47f17dd9
liruohan1436311 <u3523358@connect.hku.hk>
2020-06-12 13:02:58 -0400

    modify main function

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from network import DSSM
 from train import train
 import torch
 
-
-DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-if torch.cuda.is_available():
-    print("Current device is: %s" % torch.cuda.get_device_name(DEVICE))
-
-net = DSSM().to(DEVICE)
+NUM_EPOCHS = 5
+EPOCH_SIZE = 10
 
 def main(NUM_EPOCHS, EPOCH_SIZE):
+	DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+	if torch.cuda.is_available():
+	    print("Current device is: %s" % torch.cuda.get_device_name(DEVICE))
+	net = DSSM().to(DEVICE)
     for ep_idx in range(NUM_EPOCHS):
         train_loss = train(net, EPOCH_SIZE)
         print(str(ep_idx) + ": " + str(train_loss))
