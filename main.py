ea059b40327ec957ecad4449fd9e476d73b31565
Chipxiang <jianxiangli1996@gmail.com>
2020-06-16 13:48:42 -0400

    Uses param but not time to store different results.

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ from datetime import datetime
 from test import test
 import sys
 
-NUM_EPOCHS = sys.argv[1]
-EPOCH_SIZE = sys.argv[2]
-BATCH_SIZE = sys.argv[3]
-LEARNING_RATE = sys.argv[4]
-print(NUM_EPOCHS, EPOCH_SIZE, BATCH_SIZE, LEARNING_RATE)
+NUM_EPOCHS = int(sys.argv[1])
+EPOCH_SIZE = int(sys.argv[2])
+BATCH_SIZE = int(sys.argv[3])
+LEARNING_RATE = float(sys.argv[4])
+print("Num of epochs:", NUM_EPOCHS)
+print("Epoch size:", EPOCH_SIZE)
+print("Batch size:", BATCH_SIZE)
+print("Learning rate:", LEARNING_RATE)
 RANK = 10
 TEST_BATCH = 35
 MODEL_PATH = "/home/jianx/data/"
@@ -26,16 +29,18 @@ def main(num_epochs, epoch_size, batch_size, learning_rate, model_path, rank, te
     print("Positive Negative Pair dict size: " + str(len(pos_neg_dict)))
     print("Num of queries: " + str(len(query_dict)))
     print("Num of passages: " + str(len(passage_dict)))
-    date_time_obj = datetime.now()
-    timestamp_str = date_time_obj.strftime("%%b-%d-%Y_%H:%M:%S")
-    unique_path = model_path + timestamp_str + ".model"
-    output_path = model_path + timestamp_str + ".csv"
+    # date_time_obj = datetime.now()
+    # timestamp_str = date_time_obj.strftime("%b-%d-%Y_%H:%M:%S")
+    arg_str = str(NUM_EPOCHS) + "_" + str(EPOCH_SIZE) + "_" + str(BATCH_SIZE) + "_" + str(LEARNING_RATE)
+    unique_path = model_path + arg_str + ".model"
+    output_path = model_path + arg_str + ".csv"
     with open(output_path, mode='a') as output:
         output_writer = csv.writer(output)
         for ep_idx in range(num_epochs):
             train_loss = train(net, epoch_size, batch_size, learning_rate, dssm.device, pos_neg_dict,
                                query_dict, passage_dict)
-            avg_ndcg, avg_prec, avg_rr = test(net, test_batch, top_dict, query_test_dict, passage_dict, rating_dict, rank)
+            avg_ndcg, avg_prec, avg_rr = test(net, test_batch, top_dict, query_test_dict, passage_dict, rating_dict,
+                                              rank)
             print("Epoch:{}, loss:{}, NDCG:{}, P:{}, RR:{}".format(ep_idx, train_loss, avg_ndcg, avg_prec, avg_rr))
             output_writer.writerow([ep_idx, train_loss, avg_ndcg, avg_prec, avg_rr])
     torch.save(net, unique_path)
