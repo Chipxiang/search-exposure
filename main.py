391c27953bff23c852dd4df41866a88197941a37
Chipxiang <jianxiangli1996@gmail.com>
2020-06-16 05:01:53 -0400

    Added testing functions.

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ from network import DSSM
 from train import train
 from load_data import load
 import torch
+import csv
 from datetime import datetime
+from test import test
+import sys
 
-NUM_EPOCHS = 20
-EPOCH_SIZE = 750
-BATCH_SIZE = 1000
-LEARNING_RATE = 0.001
+NUM_EPOCHS = sys.argv[1]
+EPOCH_SIZE = sys.argv[2]
+BATCH_SIZE = sys.argv[3]
+LEARNING_RATE = sys.argv[4]
+print(NUM_EPOCHS, EPOCH_SIZE, BATCH_SIZE, LEARNING_RATE)
+RANK = 10
+TEST_BATCH = 35
 MODEL_PATH = "/home/jianx/data/"
 
 
-def main(num_epochs, epoch_size, batch_size, learning_rate, model_path):
+def main(num_epochs, epoch_size, batch_size, learning_rate, model_path, rank, test_batch):
     dssm = DSSM()
     net = dssm.to(dssm.device)
     print("Loading data")
@@ -20,15 +26,20 @@ def main(num_epochs, epoch_size, batch_size, learning_rate, model_path):
     print("Positive Negative Pair dict size: " + str(len(pos_neg_dict)))
     print("Num of queries: " + str(len(query_dict)))
     print("Num of passages: " + str(len(passage_dict)))
-    for ep_idx in range(num_epochs):
-        train_loss = train(net, epoch_size, batch_size, learning_rate, dssm.device, pos_neg_dict,
-                           query_dict, passage_dict)
-        print(str(ep_idx) + ": " + str(train_loss))
     date_time_obj = datetime.now()
     timestamp_str = date_time_obj.strftime("%%b-%d-%Y_%H:%M:%S")
     unique_path = model_path + timestamp_str + ".model"
+    output_path = model_path + timestamp_str + ".csv"
+    with open(output_path, mode='a') as output:
+        output_writer = csv.writer(output)
+        for ep_idx in range(num_epochs):
+            train_loss = train(net, epoch_size, batch_size, learning_rate, dssm.device, pos_neg_dict,
+                               query_dict, passage_dict)
+            avg_ndcg, avg_prec, avg_rr = test(net, test_batch, top_dict, query_test_dict, passage_dict, rating_dict, rank)
+            print("Epoch:{}, loss:{}, NDCG:{}, P:{}, RR:{}".format(ep_idx, train_loss, avg_ndcg, avg_prec, avg_rr))
+            output_writer.writerow([ep_idx, train_loss, avg_ndcg, avg_prec, avg_rr])
     torch.save(net, unique_path)
 
 
 if __name__ == '__main__':
-    main(NUM_EPOCHS, EPOCH_SIZE, BATCH_SIZE, LEARNING_RATE, MODEL_PATH)
+    main(NUM_EPOCHS, EPOCH_SIZE, BATCH_SIZE, LEARNING_RATE, MODEL_PATH, RANK, TEST_BATCH)
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 import torch
 import random
+import math
+import numpy as np
+from train import generate_sparse
+
 
 def test_loader(net, test_batch, top_dict, query_test_dict, passage_dict, rating_dict):
     net.eval()
@@ -22,7 +26,7 @@ def test_loader(net, test_batch, top_dict, query_test_dict, passage_dict, rating
         q_results = {}
         for j, pid in enumerate(top_list):
             p_seq = passage_dict[pid]
-            if p_seq == []:
+            if not p_seq:
                 score = -1
             else:
                 p_embed = net(generate_sparse(p_seq).to(device))
@@ -30,3 +34,56 @@ def test_loader(net, test_batch, top_dict, query_test_dict, passage_dict, rating
             q_results[pid] = score
         result_dict[qid] = q_results
     return result_dict
+
+
+def get_ndcg_precision_rr(true_dict, test_dict, rank):
+    sorted_result = sorted(test_dict.items(), key=lambda x: x[1], reverse=True)
+    rank = min(rank, len(sorted_result))
+    cumulative_gain = 0
+    ideal_dict = {}
+    num_positive = 0
+    rr = float("NaN")
+    for i in range(len(sorted_result)):
+        pid = sorted_result[i][0]
+        if pid in true_dict:
+            rr = 1 / (i + 1)
+            break
+
+    for i in range(rank):
+        pid = sorted_result[i][0]
+        relevance = 0
+        if pid in true_dict:
+            relevance = true_dict[pid]
+            num_positive += 1
+        ideal_dict[pid] = relevance
+        discounted_gain = (2 ** relevance - 1) / math.log2(2 + i)
+        cumulative_gain += discounted_gain
+    sorted_ideal = sorted(ideal_dict.items(), key=lambda x: x[1], reverse=True)
+    ideal_gain = 0
+    for i in range(rank):
+        relevance = sorted_ideal[i][1]
+        discounted_gain = (2 ** relevance - 1) / math.log2(2 + i)
+        ideal_gain += discounted_gain
+    ndcg = float("NaN")
+    if ideal_gain != 0:
+        ndcg = cumulative_gain / ideal_gain
+    return ndcg, num_positive / rank, rr
+
+
+def test(net, test_batch, top_dict, query_test_dict, passage_dict, rating_dict, rank):
+    result_dict = test_loader(net, test_batch, top_dict, query_test_dict, passage_dict, rating_dict)
+    qids = list(result_dict.keys())
+    result_ndcg = []
+    result_prec = []
+    result_rr = []
+    for qid in qids:
+        true_dict = rating_dict[qid]
+        test_dict = result_dict[qid]
+        ndcg, prec, rr = get_ndcg_precision_rr(true_dict, test_dict, rank)
+        result_ndcg.append(ndcg)
+        result_prec.append(prec)
+        result_rr.append(rr)
+    avg_ndcg = np.nanmean(result_ndcg)
+    avg_prec = np.nanmean(result_prec)
+    avg_rr = np.nanmean(result_rr)
+    return avg_ndcg, avg_prec, avg_rr
