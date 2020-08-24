e184f2561a55e76109ff5efb48802f0907eb79b7
Chipxiang <jianxiangli1996@gmail.com>
2020-08-24 04:41:20 -0400

    Random sampling

--- forward_ranker/utils.py
+++ forward_ranker/utils.py
@@ -6,3 +6,7 @@ TIME_OFFSET = -4
 
 def print_message(s, offset=TIME_OFFSET):
     print("[{}] {}".format(datetime.now(timezone(timedelta(hours=offset))).strftime("%b %d, %H:%M:%S"), s), flush=True)
+
+
+def timestamp(offset=TIME_OFFSET):
+    return datetime.now(timezone(timedelta(hours=offset))).strftime("%b_%d_%H:%M:%S")
